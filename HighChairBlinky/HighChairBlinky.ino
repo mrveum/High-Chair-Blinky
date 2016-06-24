97f9b87aa7464d720d50f1a4f046923734b9821e
Tim Bartlett <timbartlettpear@gmail.com>
2016-06-24 00:56:11 -0400

    Working update with sequencer
    Added sequencer mode 4.

--- HighChairBlinky/HighChairBlinky.ino
+++ HighChairBlinky/HighChairBlinky.ino
@@ -13,7 +13,7 @@ modes:
 1) 1 button = 1 light momentary
 2) 1 button = 1 light on until next light
 3) typewriter
-4) scrolling
+4) sequencer
 ********************************************/
 
 #include <FastLED.h>
@@ -23,9 +23,7 @@ modes:
 CRGB leds[NUM_LEDS];
 uint8_t button[] = {10, 11, 12, 13};
 uint8_t bHue[] = {0, 90, 160, 64};
-// uint8_t red[] = {255, 0, 0, 255};
-// uint8_t green[] = {0, 255, 0, 255};
-// uint8_t blue[] = {0, 0, 255, 0};
+CRGB altLeds[NUM_LEDS];
 
 bool lastButton[] = {LOW, LOW, LOW, LOW};
 bool currentButton[] = {LOW, LOW, LOW, LOW};
@@ -34,6 +32,9 @@ uint32_t prevMillis = 0;
 uint32_t ledOn = 10000; // time that local led stays on after last activity
 uint8_t mode = 1;
 uint8_t typ = 0;
+uint8_t counter = 0;
+uint16_t stepSpeed = 800; // speed for sequencer mode 4
+bool pause = false;
 
 void setup() {
   for (int p=0; p<4; p++){
@@ -61,7 +62,7 @@ void loop() {
   }
   while (mode == 4){
     modeFour();
-    switchMode();
+//    switchMode();
   }
 }
 
@@ -71,7 +72,7 @@ void modeOne(){ //one button = one light
       leds[b].setHue(bHue[b]);
     }
     else if (digitalRead(button[b]) == LOW){
-        leds[b] = CRGB::Black;
+      leds[b] = CRGB::Black;
     }
     FastLED.show();
   }
@@ -115,31 +116,68 @@ void modeThree(){ //typewriter mode
     }
     lastButton[b] = currentButton[b];
   }
-
   if (currentMillis - prevMillis == ledOn) {
-    shiftClear();
+    wipeClear();
   }
 }
 
-void modeFour(){ //scrolling
+void modeFourSetup() {
+  pause = false;
+  for (int b = 0; b < 4; b++){
+    leds[b].setHue(bHue[b]);
+    leds[b] %= 50;
+  }
+  delay(250);
   currentMillis = millis();
+  prevMillis = currentMillis;
+  typ = 0;
+  counter = 0;
+}
+
+void modeFour(){ //sequencer
+  if (typ == 4){
+    typ = 0;
+  }  
+  currentMillis = millis();
+  leds[typ].maximizeBrightness();
+  FastLED.show();
+  switchMode(); 
   for (int b = 0; b < 4; b++){
     currentButton[b] = debounce(lastButton[b], button[b]);
     if (lastButton[b] == LOW && currentButton[b] == HIGH) {
-      prevMillis = currentMillis;
-      leds[0] = leds[1];
-      leds[1] = leds[2];
-      leds[2] = leds[3];
-      leds[3].setHue(bHue[b]);
+      leds[typ].setHue(bHue[b]);
       FastLED.show();
+      counter = 0;
     }
-    lastButton[b] = currentButton[b];
   }
-  if (currentMillis - prevMillis == ledOn) {
-    for (int sl = 0; sl < 4; sl++){
-      leds[sl] = CRGB::Black;
-    }
+  if (currentMillis - prevMillis >= stepSpeed){
+    prevMillis = currentMillis;
+    leds[typ] %= 50;
     FastLED.show();
+    typ++;
+    counter++;
+  }
+  
+  if (counter >= 61) {
+    pause = true;
+    for (int p = 0; p < 4; p++){
+      altLeds[p] = leds[p];
+    }
+    clearAll();
+  }
+  while (pause == true){
+    for (int b = 0; b < 4; b++){
+      currentButton[b] = debounce(lastButton[b], button[b]);
+      if (lastButton[b] == LOW && currentButton[b] == HIGH) {
+        pause = false;
+        counter = 0;
+        for (int p = 0; p < 4; p++){
+          leds[p] = altLeds[p];
+        }
+        FastLED.show();
+        delay(150);
+      }
+    }
   }
 }
 
@@ -162,6 +200,9 @@ void switchMode(){
       }
     }
     clearAll();
+    if (mode == 4){
+      modeFourSetup();
+    }
     typ = 0;
     currentMillis = millis();
     prevMillis = currentMillis;
@@ -201,6 +242,14 @@ void shiftClear(){
     leds[2] = leds[3];
     leds[3] = CRGB::Black;
     FastLED.show();
+  }
+}
+
+void wipeClear(){
+  for (int a = 0; a < 4; a++){
+    leds[a] = CRGB::Black;
+    FastLED.show();
+    delay(200);
   }
 }
 
